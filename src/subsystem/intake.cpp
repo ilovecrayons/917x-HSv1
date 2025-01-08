1103c25233c36684da28f426266cf99131d5f05b
ilovecrayons <huangfeiyang2008@gmail.com>
2025-01-07 18:19:40 -0800

    fix: copied new features

--- src/subsystem/intake.cpp
+++ src/subsystem/intake.cpp
@@ -2,73 +2,70 @@
 #include "pros/misc.hpp"
 #include "pros/motor_group.hpp"
 
-Intake::Intake(pros::Motor& motor, pros::Optical& topSort, Arm& arm): motor(motor) , topSort(topSort) , arm(arm){};
+Intake::Intake(pros::Motor& motor, pros::Optical& topSort, Arm& arm)
+    : motor(motor)
+    , topSort(topSort)
+    , arm(arm) { };
 
-
-void Intake::setSeparation(Ring ring){
+void Intake::setSeparation(Ring ring)
+{
     this->ring = ring;
 }
 
-void Intake::checkForSort(){
-    if (ring == Ring::NONE){
-       sort = false; 
-    }
-    else if (ring == Ring::BLUE){
-        if (topSort.get_hue()>200 && topSort.get_hue()<=270){
+void Intake::checkForSort()
+{
+    if (ring == Ring::NONE) {
+        sort = false;
+    } else if (ring == Ring::BLUE) {
+        if (topSort.get_hue() > 200 && topSort.get_hue() <= 270 && topSort.get_proximity() < 100) { // TUNE PROXIMITY
             sort = true;
             INITIAL_POSITION = motor.get_position();
         }
-    }
-    else if (ring == Ring::RED){
-        if (topSort.get_hue()<30 && topSort.get_hue()>=0) {
+    } else if (ring == Ring::RED) {
+        if (topSort.get_hue() < 30 && topSort.get_hue() >= 0 && topSort.get_proximity() < 100) { // TUNE PROXIMITY
             sort = true;
             INITIAL_POSITION = motor.get_position();
         }
     }
 }
 
-void Intake::intakeControl(){
-    while(true){
+void Intake::intakeControl()
+{
+    while (true) {
 
-        if (!sort){
+        if (!sort) {
             checkForSort();
         }
-        // bool COMPLETED_MOVEMENT = (INITIAL_POSITION+SEPARATION_MOVEMENT) <= motor.get_position();
+        bool COMPLETED_MOVEMENT = (INITIAL_POSITION + SEPARATION_MOVEMENT) <= motor.get_position();
 
-        // bool COMPLETED_SEPARATION_WAIT = SEPARATION_WAIT >= TIME_TO_COMPLETE_SEP;
+        bool COMPLETED_SEPARATION_WAIT = SEPARATION_WAIT >= TIME_TO_COMPLETE_SEP;
 
-        // if (sort && !COMPLETED_MOVEMENT) {
-        //     set(IntakeState::INTAKING,90);
-        //     this->motor.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
-        // }
-        // else if (sort && COMPLETED_MOVEMENT && !COMPLETED_SEPARATION_WAIT){
-        //     this->state = STOPPED;
-        //     SEPARATION_WAIT++;
-        // }
-        // else if (sort && COMPLETED_MOVEMENT && COMPLETED_SEPARATION_WAIT) {
-        //     set(IntakeState::INTAKING,127);
-        //     sort = !sort;
-        //     SEPARATION_WAIT = 0;
-        //     this->motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-        // }
-        // if(sort){ arm.separateRing(5,true); sort = !sort; }
-        
-        
+        if (sort && !COMPLETED_MOVEMENT) {
+            set(IntakeState::INTAKING, 90);
+            this->motor.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+        } else if (sort && COMPLETED_MOVEMENT && !COMPLETED_SEPARATION_WAIT) {
+            this->state = STOPPED;
+            SEPARATION_WAIT++;
+        } else if (sort && COMPLETED_MOVEMENT && COMPLETED_SEPARATION_WAIT) {
+            set(IntakeState::INTAKING, 127);
+            sort = !sort;
+            SEPARATION_WAIT = 0;
+            this->motor.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
+        }
 
-
-        if(this->state == STOPPED){
+        if (this->state == STOPPED) {
             this->motor.brake();
-        } else if(this->state == INTAKING){
+        } else if (this->state == INTAKING) {
             this->motor.move(this->speed);
-        } else if(this->state == OUTTAKE){
+        } else if (this->state == OUTTAKE) {
             this->motor.move(-this->speed);
         }
         pros::delay(10);
     }
 }
 
-void Intake::set(IntakeState state, int speed){
+void Intake::set(IntakeState state, int speed)
+{
     this->state = state;
     this->speed = speed;
 }
-
